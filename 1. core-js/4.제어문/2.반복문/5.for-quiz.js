e80aae256aaa73ad25fcc4996958683971eb6cb4
yn0315 <yn0315@naver.com>
2021-07-04 13:25:34 +0900

    07.04 복습

--- 1. core-js/4.제어문/2.반복문/5.for-quiz.js
+++ 1. core-js/4.제어문/2.반복문/5.for-quiz.js
@@ -23,7 +23,9 @@
 var rn = Math.floor(Math.random() * 8) + 2;
 // console.log(`랜덤정수${rn}`);
 
+
 for (num1 = 1; 9 >= num1; num1++) {
-        alert(`${rn} x ${num1} = ${rn*num1}`);
-        
+        alert(rn +'x'+ num1 +'='+ rn*num1);
 }
+
+
